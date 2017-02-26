bd72d2d60c26f978889bc353479e73c2b25216b0
RocketSkates <sigal.gurman@gmail.com>
2017-02-26 18:52:14 +0200

    b

--- ExamPrep/4FromTest.c
+++ ExamPrep/4FromTest.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <string.h>
-#define N 5 
+
 
 int AlternatelySum(int num) {
 	static int x;
@@ -11,9 +10,16 @@ int AlternatelySum(int num) {
 	return (num % 10) - ((num % 100)/10) + AlternatelySum(num / 100);
 }
 
+int Is11Div(int num) {
+	if (num < 100) {
+		return (num % 10) - (num / 10);
+	}
+	return AlternatelySum(num) % 10 - AlternatelySum(num) / 10;
+}
+
 void main()
 {
 	int num = 7762348;
-	printf("%d", AlternatelySum(num));
+	printf("%d", Is11Div(num));
 
 }
